3a759cb4ed03c8db75e40088c423f2d13e2c1d48
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2024-03-20 11:52:49 -0300

    sistemas megamenu

---
+++ src/components/Navbar/components/SistemasMegamenu.jsx
@@ -0,0 +1,42 @@
+import "./megamenu.css";
+//next js
+import Link from "next/link";
+import Image from "next/image";
+//images
+import truksImage from "@/assets/gallery-image-1.webp";
+
+const SistemasMegamenu = () => {
+  const URLs = {
+    PROVEEDORES: "http://tys.yavuerp.com.ar:10000/jcnt/portal/frontend/",
+    CLIENTES: "http://logistica.terminalesyservicios.com.ar",
+  };
+  return (
+    <div className="megamenu-container">
+      <ul className="megamenu-list">
+        <li>
+          <Link href={URLs.CLIENTES} target="_blank">
+            Clientes
+          </Link>
+        </li>
+        <li>
+          <Link href={URLs.PROVEEDORES} target="_blank">
+            Proveedores
+          </Link>
+        </li>
+      </ul>
+      <div className="megamenu-info">
+        <div className="megamenu-title">Sistemas Terminales y Servicios.</div>
+        <div className="megamenu-image">
+          <Image
+            src={truksImage}
+            alt="imagend de camion"
+            placeholder="blur"
+            width={500}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SistemasMegamenu;
