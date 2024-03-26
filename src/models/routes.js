b4c9ec47b01b444ac2fa09867f9e2b5afb0c2baa
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2024-03-26 19:27:37 -0300

    proyectos model routes

--- src/models/routes.js
+++ src/models/routes.js
@@ -11,3 +11,10 @@ export const ServiciosRoutesModel = {
   SERVICIO_3: `${MainRoutesModel.SERVICIOS}/servicio-3`,
   SERVICIO_4: `${MainRoutesModel.SERVICIOS}/servicio-4`,
 };
+
+export const ProyectosRoutesModel = {
+  PROYECTO_1: `${MainRoutesModel.PROYECTOS}/proyecto-1`,
+  PROYECTO_2: `${MainRoutesModel.PROYECTOS}/proyecto-2`,
+  PROYECTO_3: `${MainRoutesModel.PROYECTOS}/proyecto-3`,
+  PROYECTO_4: `${MainRoutesModel.PROYECTOS}/proyecto-4`,
+};
