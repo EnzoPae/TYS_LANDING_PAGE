5ba52e8f22c77f6a1c69bef3e946bc9ef7e49024
EnzoPae <120670624+EnzoPae@users.noreply.github.com>
2024-04-03 12:57:54 -0300

    clients sections update

--- src/home-page/sections/Clientes/Clientes.jsx
+++ src/home-page/sections/Clientes/Clientes.jsx
@@ -2,28 +2,30 @@ import classes from "./clientes.module.css";
 import { ClientesLogos } from "./Logos";
 
 const Clientes = () => {
+  const lorem =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
   return (
     <section
       style={{ backgroundColor: "#fff", paddingTop: 100, paddingBottom: 100 }}
     >
-      <div className="g-p-left g-p-right flex justify-content-center">
-        <div className={`${classes.hero} flex`}>
-          <h3 className="color-secondary fs-3">¿Por qué elegirnos?</h3>
-          <p className="color-gray-dark">
-            La calidad es el distintivo de nuestro enfoque. Desde la concepción
-            de nuestras ideas hasta la entrega final, cada paso de nuestro
-            proceso se realiza con una atención meticulosa a los detalles.
-            Nuestro equipo de profesionales altamente capacitados trabaja
-            incansablemente para garantizar que cada producto/servicio no solo
-            cumpla, sino que supere las expectativas más exigentes.
-          </p>
-        </div>
-      </div>
-      <div className="w-100 overflow-hidden">
+      <h3
+        className="color-secondary g-p-left g-p-right fs-3 mb-2"
+        style={{ textAlign: "center" }}
+      >
+        ¿Quienes nos eligen?
+      </h3>
+      <div className="w-100 overflow-hidden mt-2">
         <div className={`${classes.logos} flex`}>
           <ClientesLogos />
           <ClientesLogos />
         </div>
+      </div>
+      <div className={`${classes.text_box} color-gray-dark g-p-left g-p-right`}>
+        <p>{lorem}</p>
+        <div className={classes.divider}></div>
+        <p>{lorem}</p>
+        <div className={classes.divider}></div>
+        <p>{lorem}</p>
       </div>
     </section>
   );
